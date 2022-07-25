820374e0306ded6201054efdd891e5e6af631a05
kuznar <107443276+kuznar@users.noreply.github.com>
2022-07-25 19:48:26 -0400

    add v7 data

--- plot.py
+++ plot.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data/v6.csv", index_col=1)
+version='v7'
+
+data = pd.read_csv(f"data/{version}.csv", index_col=1)
 
 ax=data[data['episode']==0][['reward']].plot()
 ax = data[data['episode']==1][['reward']].plot(ax=ax)
 data[data['episode']==2][['reward']].plot(ax=ax)
 
-plt.savefig("charts/v6.png")
+plt.savefig(f"charts/{version}.png")
