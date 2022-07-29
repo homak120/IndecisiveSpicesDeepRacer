773aca0e2aacac5a663b2265bd675b785583892e
kuznar <107443276+kuznar@users.noreply.github.com>
2022-07-28 21:01:22 -0400

    format files

--- plotPath.py
+++ plotPath.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#change me to data csv filename. Uses evaluation data not training data.
-version='v12'
+# change me to data csv filename. Uses evaluation data not training data.
+version = 'v12'
 
 data = pd.read_csv(f"data/{version}.csv", index_col=1)
 
-ax = data[data['episode']==0].plot(x='X', y='Y')
-data[data['episode']==1].plot(x='X', y='Y', ax=ax)
-data[data['episode']==2].plot(x='X', y='Y', ax=ax)
+ax = data[data['episode'] == 0].plot(x='X', y='Y')
+data[data['episode'] == 1].plot(x='X', y='Y', ax=ax)
+data[data['episode'] == 2].plot(x='X', y='Y', ax=ax)
 
 plt.savefig(f"charts/{version}path.png")
--- plotReward.py
+++ plotReward.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#change me to data csv filename. Uses evaluation data not training data.
-version='v12'
+# change me to data csv filename. Uses evaluation data not training data.
+version = 'v12'
 
 data = pd.read_csv(f"data/{version}.csv", index_col=1)
 
-ax = data[data['episode']==0][['reward']].plot()
-ax =data[data['episode']==1][['reward']].plot(ax=ax)
-data[data['episode']==2][['reward']].plot(ax=ax)
+ax = data[data['episode'] == 0][['reward']].plot()
+ax = data[data['episode'] == 1][['reward']].plot(ax=ax)
+data[data['episode'] == 2][['reward']].plot(ax=ax)
 
 plt.savefig(f"charts/{version}Reward.png")
